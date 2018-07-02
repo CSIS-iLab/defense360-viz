ab8dbc172b6acc1b2c0fcc70bd5155cf3a40b6ee
Jacque Schrag <jnschrag@gmail.com>
2018-07-02 17:14:53 -0400

    Add layer styles to NATO map. Switch between layers via dropdown.

--- defense-metrics/d360euro_map/js/app.js
+++ defense-metrics/d360euro_map/js/app.js
@@ -1,55 +1,84 @@
 const map = L.map('map').setView([50, 15], 4);
 
-// Adding Voyager Basemap
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+// https://api.mapbox.com/styles/v1/ilabmedia/cjj4grwom04892sp39x48dlvn/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw
+// Voyager
+L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjj4grwom04892sp39x48dlvn/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {
   maxZoom: 18
 }).addTo(map);
 
-// Adding Voyager Labels
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png', {
+// Labels only: https://api.mapbox.com/styles/v1/ilabmedia/cjj4psbcp4hzb2sqb7iuo2njc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw
+// Vorager labels only: https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png
+L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjj4psbcp4hzb2sqb7iuo2njc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {
   maxZoom: 18,
-  zIndex: 10
+  zIndex: 100
 }).addTo(map);
 
 var client = new carto.Client({
-  apiKey: 'RoNKupXWKtMngcY5aciIdQ',
+  apiKey: 'wb6BQbxW-Z_a3tVYS26J7Q',
   username: 'csis'
 });
 
-const europeanCountriesDataset0 = new carto.source.Dataset(`
-  ne_adm0_europe_2
-`);
-const europeanCountriesStyle0 = new carto.style.CartoCSS(`
+const natoCountriesData = new carto.source.Dataset('defense_metrics_nato_map');
+const natoCountriesStyles = new carto.style.CartoCSS(`
   #layer {
-  polygon-fill: #162945;
     polygon-opacity: 0.5;
-    ::outline {
-      line-width: 1;
-      line-color: #FFFFFF;
-      line-opacity: 0.5;
-    }
   }
 `);
-const europeanCountries0 = new carto.layer.Layer(europeanCountriesDataset0, europeanCountriesStyle0);
+const natoCountriesLayer = new carto.layer.Layer(natoCountriesData, natoCountriesStyles);
 
-const europeanCountriesDataset1 = new carto.source.SQL(`
-  SELECT *
-    FROM ne_adm0_europe_2
-    WHERE admin IN (SELECT field_1 FROM ground_mobility_rating_sheet1_1 WHERE spod=1)
-`);
-const europeanCountriesStyle1 = new carto.style.CartoCSS(`
-  #layer {
-  polygon-fill: #647A32;
-    polygon-opacity: 0.5;
-    ::outline {
-      line-width: 1;
-      line-color: #FFFFFF;
-      line-opacity: 0.5;
-    }
-  }
-`);
-const europeanCountries1 = new carto.layer.Layer(europeanCountriesDataset1, europeanCountriesStyle1);
-
-
-client.addLayers([europeanCountries1]);
+client.addLayers([natoCountriesLayer]);
 client.getLeafletLayer().addTo(map);
+
+const popup = L.popup({ closeButton: false });
+natoCountriesLayer.on(carto.layer.events.FEATURE_OVER, featureEvent => {
+  console.log(featureEvent)
+  popup.setContent(``);
+  if (!popup.isOpen()) {
+    popup.openOn(map);
+  }
+});
+
+natoCountriesLayer.on(carto.layer.events.FEATURE_OUT, featureEvent => {
+  popup.removeFrom(map);
+});
+
+/*----------  Shading  ----------*/
+const mobilitySelector = document.querySelector('.js-mobility')
+mobilitySelector.addEventListener("change", function() {
+  let layer = layerStyles[this.value]
+  updateLayerStyles(layer)
+})
+
+// const colors = '#e6e56b,#B9DE73,#84C076,#5BA075,#41806C,#365F5A';
+const colors = '#cee0de, #a8c0bd, #82a09c, #5c7f7b, #365F5A';
+// const colors = '#96b586, #7ea07b, #668a70, #4e7465, #4e7465';
+const partnerCountries = `
+  [is_partner = 1] {
+    polygon-pattern-file: url(http://com.cartodb.users-assets.production.s3.amazonaws.com/patterns/diagonal_1px_med.png);
+    polygon-pattern-opacity: 0.2;
+    polygon-pattern-alignment: global;
+     line-color: #FFFFFF;
+     line-width: 0.5;
+     line-opacity: 1;
+     line-dasharray: 10, 5;
+  }
+`;
+const layerStyles = {
+  overall: 'mobility_score',
+  ground: 'ground_mobility',
+  air: 'air_mobility'
+}
+
+function updateLayerStyles(layer) {
+  natoCountriesStyles.setContent(`#layer {
+    polygon-fill: ramp([${layer}], (${colors}), quantiles);
+    ::outline {
+      line-color: #fff;
+      line-width: 0.75;
+    }
+    ${partnerCountries}
+  }`);
+}
+
+updateLayerStyles(layerStyles.overall)
+
