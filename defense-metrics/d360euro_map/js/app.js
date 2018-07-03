0e4b2949aeb4ae27548ea01a49ecd416c90ff870
Jacque Schrag <jnschrag@gmail.com>
2018-07-03 13:55:11 -0400

    Update map.

--- defense-metrics/d360euro_map/js/app.js
+++ defense-metrics/d360euro_map/js/app.js
@@ -2,16 +2,16 @@ const map = L.map('map').setView([50, 15], 4);
 
 // https://api.mapbox.com/styles/v1/ilabmedia/cjj4grwom04892sp39x48dlvn/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw
 // Voyager
-L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjj4grwom04892sp39x48dlvn/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {
+L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjj5wwc2o0x3o2so08xb67sgz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {
   maxZoom: 18
 }).addTo(map);
 
 // Labels only: https://api.mapbox.com/styles/v1/ilabmedia/cjj4psbcp4hzb2sqb7iuo2njc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw
 // Vorager labels only: https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}.png
-L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjj4psbcp4hzb2sqb7iuo2njc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {
-  maxZoom: 18,
-  zIndex: 100
-}).addTo(map);
+// L.tileLayer('https://api.mapbox.com/styles/v1/ilabmedia/cjj4psbcp4hzb2sqb7iuo2njc/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWxhYm1lZGlhIiwiYSI6ImNpbHYycXZ2bTAxajZ1c2tzdWU1b3gydnYifQ.AHxl8pPZsjsqoz95-604nw', {
+//   maxZoom: 18,
+//   zIndex: 50
+// }).addTo(map);
 
 var client = new carto.Client({
   apiKey: 'wb6BQbxW-Z_a3tVYS26J7Q',
@@ -24,23 +24,51 @@ const natoCountriesStyles = new carto.style.CartoCSS(`
     polygon-opacity: 0.5;
   }
 `);
-const natoCountriesLayer = new carto.layer.Layer(natoCountriesData, natoCountriesStyles);
 
-client.addLayers([natoCountriesLayer]);
+const featureColumns = ['country_name', 'gdp_percentage', 'gdp_raw', 'defense_budget', 'equipment_percentage', 'total_active_duty_force', 'active_us_troops', 'mobility_score', 'ground_mobility', 'air_mobility']
+
+const natoCountriesLayer = new carto.layer.Layer(natoCountriesData, natoCountriesStyles, {
+  featureOverColumns: featureColumns
+});
+
+// % spent of GDP on defense, their defense budget, the % of defense budget spent on equipment, and the size of their active duty force
+
+client.addLayer(natoCountriesLayer);
 client.getLeafletLayer().addTo(map);
 
 const popup = L.popup({ closeButton: false });
 natoCountriesLayer.on(carto.layer.events.FEATURE_OVER, featureEvent => {
-  console.log(featureEvent)
-  popup.setContent(``);
+  let data = featureEvent.data
+  popup.setLatLng(featureEvent.latLng);
+  popup.setContent(`
+    <span class="country-name">${data.country_name}</span>
+    <span class="label">2017 % GDP on Defense:</span> 
+    ${data.gdp_percentage}%<br />
+    <span class="label">2017 GDP (2010 US$):</span> 
+    $${data.gdp_raw}B<br />
+    <span class="label">Defense Budget (2010 US$):</span> 
+    $${data.defense_budget}B<br />
+    <span class="label">2017 Equipment %:</span> 
+    ${data.equipment_percentage}%<br />
+    <span class="label">2017 Total Active Duty Force:</span> 
+    ${data.total_active_duty_force.toLocaleString()}<br />
+    <span class="label">2018 Active Duty U.S. Troops in Country:</span> 
+    ${data.active_us_troops.toLocaleString()}<br />
+    <span class="label">Overall Mobility:</span> 
+    ${data.mobility_score} <span class="label">/ 24</span><br />
+    <span class="label">Ground Mobility:</span> 
+    ${data.ground_mobility} <span class="label">/ 3</span><br />
+    <span class="label">Air Mobility:</span> 
+    ${data.air_mobility} <span class="label">/ 4</span><br />
+  `);
   if (!popup.isOpen()) {
     popup.openOn(map);
   }
 });
 
-natoCountriesLayer.on(carto.layer.events.FEATURE_OUT, featureEvent => {
-  popup.removeFrom(map);
-});
+// natoCountriesLayer.on(carto.layer.events.FEATURE_OUT, featureEvent => {
+//   popup.removeFrom(map);
+// });
 
 /*----------  Shading  ----------*/
 const mobilitySelector = document.querySelector('.js-mobility')
@@ -49,15 +77,13 @@ mobilitySelector.addEventListener("change", function() {
   updateLayerStyles(layer)
 })
 
-// const colors = '#e6e56b,#B9DE73,#84C076,#5BA075,#41806C,#365F5A';
-const colors = '#cee0de, #a8c0bd, #82a09c, #5c7f7b, #365F5A';
-// const colors = '#96b586, #7ea07b, #668a70, #4e7465, #4e7465';
+const colors = '#fbe4a3, #d0d197, #96b586, #4ba292,#008e9d';
 const partnerCountries = `
   [is_partner = 1] {
-    polygon-pattern-file: url(http://com.cartodb.users-assets.production.s3.amazonaws.com/patterns/diagonal_1px_med.png);
-    polygon-pattern-opacity: 0.2;
+    polygon-pattern-file: url(https://i.imgur.com/k3J0pnR.png);
+    polygon-pattern-opacity: 0.4;
     polygon-pattern-alignment: global;
-     line-color: #FFFFFF;
+     line-color: #dfe5e7;
      line-width: 0.5;
      line-opacity: 1;
      line-dasharray: 10, 5;
@@ -73,8 +99,8 @@ function updateLayerStyles(layer) {
   natoCountriesStyles.setContent(`#layer {
     polygon-fill: ramp([${layer}], (${colors}), quantiles);
     ::outline {
-      line-color: #fff;
-      line-width: 0.75;
+      line-color: #dfe5e7;
+      line-width: 1;
     }
     ${partnerCountries}
   }`);
