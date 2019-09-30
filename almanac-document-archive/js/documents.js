76b866420326d7529b6c1382ba556ce85bcc9a70
Jacque Schrag <jnschrag@gmail.com>
2019-09-30 17:09:08 -0400

    Remove height depenency on message event

--- almanac-document-archive/js/documents.js
+++ almanac-document-archive/js/documents.js
@@ -1,23 +1,19 @@
-window.addEventListener('message', function(event) {
+window.addEventListener("message", function(event) {
   // get defense system name from page title via postMessage api in onload event on iframe
-  if (event.origin.indexOf('google') > -1) return
-  var defense_system = event.data
-    .replace(' | Defense360', '')
-    .replace(/(_|%20)/g, ' ')
-
-  callChart(defense_system)
-})
+  if (event.origin.indexOf("google") > -1) return;
+  callChart(event.data);
+});
 
 function callChart(defense_system) {
-  const SPREADSHEET_ID = '1AkJfA3GZbXsAlZIpsc34rXrYuYlVgzspa0ml9Xe7KpE'
+  const SPREADSHEET_ID = "1AkJfA3GZbXsAlZIpsc34rXrYuYlVgzspa0ml9Xe7KpE";
 
-  gapi.load('client', function() {
+  gapi.load("client", function() {
     gapi.client
       .init({
-        apiKey: 'AIzaSyBukM0ddC8qPCIJvhE3ZXyDMnXRELLTb8k',
-        scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
+        apiKey: "AIzaSyBukM0ddC8qPCIJvhE3ZXyDMnXRELLTb8k",
+        scope: "https://www.googleapis.com/auth/spreadsheets.readonly",
         discoveryDocs: [
-          'https://sheets.googleapis.com/$discovery/rest?version=v4'
+          "https://sheets.googleapis.com/$discovery/rest?version=v4"
         ]
       })
       .then(function() {
@@ -27,31 +23,26 @@ function callChart(defense_system) {
             range: "'" + defense_system + "'!A:Z"
           })
           .then(function(sheet) {
-            renderTable({ data: sheet.result.values })
-          })
-      })
-  })
+            renderTable({ data: sheet.result.values });
+          });
+      });
+  });
 }
 
 function renderTable(sheet) {
-  $('#documents').DataTable({
+  $("#documents").DataTable({
     data: sheet.data.map(function(r) {
       return [
         r[0],
         r[1],
         r[2],
         '<a href="' + r[3] + '" target="_blank" class="btn">VIEW</button></a>'
-      ]
+      ];
     }),
     columns: sheet.data[0].map(function(c) {
-      return { title: c }
+      return { title: c };
     }),
     fixedHeader: true,
-    responsive: { details: false },
-    initComplete: function() {
-      var bodyHeight = document.body.scrollHeight + 20
-
-      window.parent.postMessage(bodyHeight, '*')
-    }
-  })
+    responsive: { details: false }
+  });
 }
