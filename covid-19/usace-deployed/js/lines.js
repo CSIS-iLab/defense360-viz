49f56c1addf72575e81dbca21eba74f506914d31
mariel-delagarza <mariel.code@gmail.com>
2020-04-28 13:52:29 -0400

    Copied over code from first chart

---
+++ covid-19/usace-deployed/js/lines.js
@@ -0,0 +1,74 @@
+Highcharts.setOptions({
+  lang: {
+      numericSymbols: null 
+  }
+});
+
+$(function() {
+  $('#hcContainer').highcharts({
+    // Load Data in from Google Sheets
+    data: {
+      googleSpreadsheetKey: '1OzJwZ8wTlflcKsb_yKfmGchhkAzUkQ-fexGwY-c-sAk',
+      googleSpreadsheetWorksheet: 1
+    },
+    // General Chart Options
+    chart: {
+      zoomType: 'x',
+      type: 'line'
+    },
+    // Colors
+    colors: [
+      '#83373E',
+      '#365F5A'
+      
+    ],
+    // Chart Title and Subtitle
+    title: {
+      text: "Active and Reserve Personnel in COVID-19 Response"
+    },
+    subtitle: {
+      text: "Click and drag to zoom in"
+    },
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS Defense360 | Source: Department of Defense"
+    },
+    // Chart Legend
+    legend: {
+      align: 'center',
+      verticalAlign: 'bottom',
+      layout: 'horizontal'
+    },
+    // Y Axis
+    yAxis: {
+      title: {
+        text: "Number of Personnel"
+      }
+    },
+    xAxis: {
+      dateTimeLabelFormats: {
+        day: '%b %e'
+      }
+    },
+    // Tooltip
+    tooltip: {
+        shared: true,
+        useHTML: true,
+        xDateFormat: '%B %e'
+    },    
+    // Additional Plot Options
+    plotOptions:
+    {
+      line: {
+        marker: {
+          enabled: false,
+          symbol: "circle",
+          radius: 3
+        },
+        lineWidth: 3
+      }
+    }
+  });
+});
