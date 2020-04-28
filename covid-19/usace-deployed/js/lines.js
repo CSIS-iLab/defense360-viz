5192db71fb77ceb8026b6d5f130060eb5a1718c7
mariel-delagarza <mariel.code@gmail.com>
2020-04-28 13:53:43 -0400

    Update chart and page title

--- covid-19/usace-deployed/js/lines.js
+++ covid-19/usace-deployed/js/lines.js
@@ -24,7 +24,7 @@ $(function() {
     ],
     // Chart Title and Subtitle
     title: {
-      text: "Active and Reserve Personnel in COVID-19 Response"
+      text: "Army Corps of Engineers Personnel Deployed"
     },
     subtitle: {
       text: "Click and drag to zoom in"
