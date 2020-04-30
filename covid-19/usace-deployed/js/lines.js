a60be3c3f634f963b4b1f55f9fab159d38eb782c
mariel-delagarza <mariel.code@gmail.com>
2020-04-30 14:04:43 -0400

    Update y-axis title

--- covid-19/usace-deployed/js/lines.js
+++ covid-19/usace-deployed/js/lines.js
@@ -42,7 +42,7 @@ $(function() {
     // Y Axis
     yAxis: {
       title: {
-        text: "Number of Personnel"
+        text: "Number of Missions and AFC Contracts"
       },
       labels: {
         formatter: function() {
