415f08e5ed2602c38ac3f3971e759290b0ad753b
Mariel <mariel.code@gmail.com>
2020-06-11 10:26:50 -0400

    Covid 19 update (#32)
    * Initial draft of chart
    
    * Remove markers and line label
    
    * Change x-axis settings so dates not quished on mobile
    
    * Change colors
    
    * Remove unneeded code, force zoom option
    
    * Change colors
    
    * Update title
    
    * Remove duplicate word in title

---
+++ covid-19/new-cases/js/combination.js
@@ -0,0 +1,70 @@
+Highcharts.chart('hcContainer', {
+  data: {
+    googleSpreadsheetKey: '16cmspkQOI6riyOWyXc7ZkafRpWqYtMIMn3rHZeBoQ3Q',
+    googleSpreadsheetWorksheet: 1,
+    startColumn: 0,
+    endColumn: 3,
+    dateFormat: 'mm/dd/YYYY'
+  },
+  chart: {
+    type: 'column',
+    zoomType: 'xy'
+  },
+  colors: ['#D05F4C', '#365F5A'],
+  title: {
+    text: 'New DoD Cases of Covid-19'
+  },
+  subtitle: {
+    text: "Click and drag to zoom in"
+  },
+  credits: {
+    enabled: true,
+    href: false,
+    text: "CSIS Defense360 | Source: DoD Covid-19 Updates"
+  },
+  legend: {
+    align: 'center',
+    verticalAlign: 'bottom',
+    layout: 'horizontal'
+  },
+  yAxis: {
+    title: {
+      text: "Number of Cases"
+    }
+  },
+  xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: {
+        day: '%e-%b',
+        week: '%e-%b'
+      }
+  },
+  tooltip: {
+    shared: true,
+    useHTML: true,
+    valueDecimals: 0,
+    xDateFormat: '%b %e, %Y'
+  },
+  series: [{
+    type: 'column',
+    visible: false,
+    showInLegend: false
+  }, {
+    type: 'column'
+  }, {
+    type: 'line',
+    label: {
+      enabled: false
+    }
+  }],
+  plotOptions:{
+    line: {
+      marker: {
+        enabled: false,
+        symbol: "circle",
+        radius: 3
+      },
+      lineWidth: 3
+    }
+  }
+});
